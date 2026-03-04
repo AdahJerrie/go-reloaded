86fb7f80078bd7d30fc12cbed753ab9bae8828ff
AdahJerrie <adahjerry51@gmail.com>
2026-03-04 05:04:27 +0100

    test for hex

--- modifiers/todecimal_test.go
+++ modifiers/todecimal_test.go
@@ -3,10 +3,10 @@ package modifiers
 import "testing"
 
 func TestHex(t *testing.T) {
-	input := "It has been 10 (bin) years."
+	input := "It has been 10(bin) years."
 	expected := "It has been 2 years."
 
-	outcome := ReplaceNum(input)
+	outcome := ToDecimal(input)
 
 	if outcome != expected {
 		t.Errorf("Actual-Result is %s, Expected-Result is %s\n", outcome, expected)
